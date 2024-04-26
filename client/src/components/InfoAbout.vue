766b83d6f83f37493f1af66c6def2b84f95f944f
Marc Schlagenhauf <marc@schlagenhauf.net>
2024-04-26 14:22:23 +0200

    Add info dialog

---
+++ client/src/components/InfoAbout.vue
@@ -0,0 +1,49 @@
+<template>
+    <p>&copy; 2023&ndash;2024 Marc Schlagenhauf</p>
+    <p class="mb-2">AGPLv3+ | <a href="https://github.com/ref-it/infoscreen" target="_blank">https://github.com/ref-it/infoscreen</a></p>
+
+    <p class="mb-2"><a href="https://www.stura-ilmenau.de/impressum" target="_blank">Impressum</a> | <a href="https://www.stura-ilmenau.de/datenschutz" target="_blank">Datenschutz</a></p>
+
+    <b>Quellen:</b>
+    <ul class="mb-2">
+        <li>
+            <b>Speisepläne:</b><br />
+            <a href="https://www.stw-thueringen.de/mensen/" target="_blank">Studierendenwerk Thüringen</a><br />
+            (Vielen Dank für die freundliche Genehmigung der Nutzung!)
+        </li>
+        <li>
+            <b>Abfahrtsdaten (Zug):</b><br />
+            <a href="https://developers.deutschebahn.com/" target="_blank">Deutsche Bahn AG</a><br />
+            (Creative Commons Attribution 4.0 International)
+        </li>
+        <li>
+            <b>Abfahrtsdaten (Camil):</b><br />
+            <a href="https://www.camil-ilmenau.de/mitfahren" target="_blank">IOV Omnibusverkehr GmbH</a>
+        </li>
+        <li>
+            <b>Wetterdaten:</b><br />
+            <a href="https://dwd.api.bund.dev/" target="_blank">Deutscher Wetterdienst (OpenData)</a>
+        </li>
+        <li>
+            <b>Warnungen:</b><br />
+            <a href="https://nina.api.bund.dev/" target="_blank">Bundesamt für Bevölkerungsschutz (OpenData)</a>
+        </li>
+    </ul>
+
+    <b>Bilder:</b>
+    <ul>
+        <li>
+            <b>Wetter:</b><br />
+            <a href="https://github.com/erikflowers/weather-icons" target="_blank">https://github.com/erikflowers/weather-icons</a><br />
+            (SIL OFL 1.1)
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+li {
+    list-style: disc;
+    margin-left: 1.5rem;
+    margin-bottom: .25rem;
+}
+</style>
