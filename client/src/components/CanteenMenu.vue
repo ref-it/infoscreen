c9ee666bced61985c36c28908a56abca2f458dfb
Marc Schlagenhauf <marc@schlagenhauf.net>
2024-05-22 13:47:47 +0200

    Smaller meal title text

--- client/src/components/CanteenMenu.vue
+++ client/src/components/CanteenMenu.vue
@@ -12,7 +12,7 @@
                 <div class="px-4 py-2 pb-3 border-l-4" :class="(meal.isVegetarian && !meal.isVegan)?'border-lime-400':'' || (meal.isVegan)?'border-green-700':'' || (!meal.isVegetarian && !meal.isVegan)?'border-zinc-800':''">
                     <div class="grid meal-item">
                         <div class="pr-3 border-r border-zinc-700">
-                            <div class="text-xl mb-2 font-bold">{{ meal.name }}</div>
+                            <div class="text-base mb-2 font-bold">{{ meal.name }}</div>
                             <div v-if="meal.additives.length > 0" class="text-sm"><b>Z:</b> <span>{{ meal.additives[0] }}</span><span v-for="(additive, index) in meal.additives.slice(1)" :key="index">, {{ additive }}</span></div>
                             <div v-if="meal.allergens.length > 0" class="text-sm"><b>A:</b> <span>{{ meal.allergens[0] }}</span><span v-for="(allergen, index) in meal.allergens.slice(1)" :key="index">, {{ allergen }}</span></div>
                         </div>
@@ -30,7 +30,7 @@
                 <div class="px-4 py-2 pb-3 border-l-4" :class="(meal.isVegetarian && !meal.isVegan)?'border-lime-400':'' || (meal.isVegan)?'border-green-700':'' || (!meal.isVegetarian && !meal.isVegan)?'border-zinc-800':''">
                     <div class="grid meal-item">
                         <div class="pr-3 border-r border-zinc-700">
-                            <div class="text-xl font-bold">{{ meal.name }}</div>
+                            <div class="text-base font-bold">{{ meal.name }}</div>
                             <div v-if="meal.additives.length > 0" class="text-sm"><b>Z:</b> <span>{{ meal.additives[0] }}</span><span v-for="(additive, index) in meal.additives.slice(1)" :key="index">, {{ additive }}</span></div>
                             <div v-if="meal.allergens.length > 0" class="text-sm"><b>A:</b> <span>{{ meal.allergens[0] }}</span><span v-for="(allergen, index) in meal.allergens.slice(1)" :key="index">, {{ allergen }}</span></div>
                         </div>
